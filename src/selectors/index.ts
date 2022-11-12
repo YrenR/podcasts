354d8b5c2b1a6b77fdd1b08d375c4f387e5c7d3a
YrenR <yren.rivero@gmail.com>
2022-11-12 18:21:04 +0100

    feat: selecTopPodcasts

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,3 +1,4 @@
 import { RootState } from "../store/types";
 
 export const selectCount = (state: RootState) => state.counter.value;
+export const selectTopPodcasts = (state: RootState) => state.podcasts;
