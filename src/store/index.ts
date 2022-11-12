95e6ef64ae00748bbdaba68a5f632f940b504e61
YrenR <yren.rivero@gmail.com>
2022-11-12 11:05:06 +0100

    chore: restructure folders

---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import counterReducer from "./slices/counterSlice";
+
+const reducers = combineReducers({ counter: counterReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        isSerializable: () => true,
+      },
+    }).concat(thunk),
+});
+
+export const persistor = persistStore(store);
