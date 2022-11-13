c51df6ce91d8b319def3a1ab96edd589689290fa
YrenR <yren.rivero@gmail.com>
2022-11-13 18:02:32 +0100

    feat: add new endpoint api

--- src/httpClients/podcastApi.ts
+++ src/httpClients/podcastApi.ts
@@ -1,15 +1,25 @@
 import { ApiError, axiosInstance } from "../libs/axios";
+import { Episodes } from "../models/episode";
 import { TopPodcast } from "../models/podcast";
 
-interface params {
-  limit: number;
-  genre: number;
-}
-
-export const getTopPodasts = ({ limit, genre }: params): Promise<TopPodcast> => {
+export const getTopPodasts = ({ limit, genre }: { limit: number; genre: number }): Promise<TopPodcast> => {
   return new Promise((resolve, reject) => {
     axiosInstance
       .get<TopPodcast>(`/us/rss/toppodcasts/limit=${limit}/genre${genre}/json`)
+      .then(({ data }) => {
+        resolve(data);
+      })
+      .catch((error: ApiError) => {
+        console.error(error);
+        reject(error);
+      });
+  });
+};
+
+export const getEpisodes = ({ podcastId }: { podcastId: string }): Promise<Episodes> => {
+  return new Promise((resolve, reject) => {
+    axiosInstance
+      .get<Episodes>(`/lookup?id=${podcastId}&entity=podcastEpisode`)
       .then(({ data }) => {
         resolve(data);
       })
