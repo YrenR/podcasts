d677669d6ffe4cbe96f6214b2aca4ea60e2c17dc
YrenR <yren.rivero@gmail.com>
2022-11-13 18:02:59 +0100

    feat: aux methods

--- src/tools/util.ts
+++ src/tools/util.ts
@@ -7,3 +7,24 @@ export const addMilliseconds = (date: Date, ms: number): Date => {
 export const isExpired = (date: Date | string): boolean => {
   return new Date(date) < new Date();
 };
+
+export const padTo2Digits = (num: number) => {
+  return num.toString().padStart(2, "0");
+};
+
+export const normalizeMillisecondsToMinSec = (milliseconds: number) => {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+
+  return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+};
+
+export const formatDateDDMMYYYY = (date: Date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${padTo2Digits(day)}/${padTo2Digits(month)}/${year}`;
+};
