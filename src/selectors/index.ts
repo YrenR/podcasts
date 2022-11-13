f48676c8e83c502546efb54ea5fe219333005227
YrenR <yren.rivero@gmail.com>
2022-11-13 18:05:07 +0100

    chore: add select helper store

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -2,3 +2,7 @@ import { RootState } from "../store/types";
 
 export const selectCount = (state: RootState) => state.counter.value;
 export const selectPodcasts = (state: RootState) => state.podcasts;
+export const selectTopPodcasts = (state: RootState) => state.podcasts.topPodcast;
+export const selectEpisodes = (state: RootState) => state.podcasts.episodes;
+export const selectEpisodeByPodcastId = (state: RootState, podcastId?: string) =>
+  podcastId ? state.podcasts.episodes[podcastId] : undefined;
