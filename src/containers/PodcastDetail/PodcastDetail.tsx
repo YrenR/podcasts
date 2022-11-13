76d3a7b6a04a797daa0a1a12ea439e688fc3597c
YrenR <yren.rivero@gmail.com>
2022-11-13 18:06:36 +0100

    feat: PodcastDetails screen with component

--- src/containers/PodcastDetail/PodcastDetail.tsx
+++ src/containers/PodcastDetail/PodcastDetail.tsx
@@ -1,14 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { useParams } from "react-router-dom";
+import { selectEpisodeByPodcastId, selectPodcasts } from "../../selectors";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import PodcastSidebar from "./components/PodcastSidebar/PodcastSidebar";
+import { getEpisodesAsync } from "../../store/slices/podcastSlice";
+import EpisodesTable from "./components/EpisodesTable/EpisodesTable";
+
+type PodcastDetailParams = { podcastId: string };
 
 const PodcastDetail = () => {
+  const { podcastId } = useParams<PodcastDetailParams>();
+  const { topPodcast } = useAppSelector(selectPodcasts);
+  const episodes = useAppSelector((state) => selectEpisodeByPodcastId(state, podcastId));
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (podcastId) dispatch(getEpisodesAsync(podcastId));
+  }, [dispatch, podcastId]);
+
+  const podcasts = topPodcast.response?.feed.entry;
+  const podcast = podcasts?.find(({ id }) => id.attributes["im:id"] === podcastId);
+
   return (
-    <>
-      <h1>PodcastDetail</h1>
-      <Link to="/">podcast list</Link>
-      <Link to="/podcast/1">podcast detail</Link>
-      <Link to="/podcast/1/episode/1">episode</Link>
-    </>
+    <Grid container padding={8} justifyContent="space-around">
+      <Grid item xs={12} sm={4} md={2}>
+        <PodcastSidebar podcast={podcast} />
+      </Grid>
+      <Grid item xs={12} sm={6} md={8}>
+        <Card>
+          <CardContent>
+            <Typography fontWeight="bold">Episodes: {episodes?.resultCount}</Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ marginTop: 5 }}>
+          <CardContent>
+            <EpisodesTable episodes={episodes?.results} podcastId={podcastId} />
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
